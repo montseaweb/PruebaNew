d1f29f10c4ca5fb324a8cc2252a531ab77a26873
Montse Viyuela <montsemsanz@gmail.com>
2020-10-10 20:27:24 +0200

    Intercambio inicial de proyecto

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Main
+{
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world Hola mundo");
+
+        int a = 10;
+
+    }
+}
